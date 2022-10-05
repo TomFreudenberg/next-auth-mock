df5f1a55e82fca8a182402b39c1ec216f47758a7
Tom Freudenberg <tom.freudenberg@4commerce.de>
2022-10-06 01:20:59 +0200

    mod: rewrite storybook component, easier integration

--- src/storybook/index.js
+++ src/storybook/index.js
@@ -1,88 +1,41 @@
+import React from 'react';
 
-export const mockPreviewAuthentications = {
-  unknown: {
-    title: 'session unknown',
-    session: null
-  },
-  loading: {
-    title: 'session loading',
-    session: {
-      data: null,
-      status: 'loading'
-    }
-  },
-  admin: {
-    title: 'admin not authenticated',
-    session: {
-      data: {
-        id: 1,
-        login: 'admin',
-        role: 'admin',
-        roles: ['admin', 'user'],
-        username: 'Administrator',
-        email: 'admin@local'
-      },
-      status: 'unauthenticated'
-    }
-  },
-  adminAuthed: {
-    title: 'admin authenticated',
-    session: {
-      data: {
-        id: 1,
-        login: 'admin',
-        role: 'admin',
-        roles: ['admin', 'user'],
-        username: 'Administrator',
-        email: 'admin@local'
-      },
-      status: 'authenticated'
-    }
-  },
-  user: {
-    title: 'user not authenticated',
-    session: {
-      data: {
-        id: 999,
-        login: 'user',
-        role: 'user',
-        roles: ['user'],
-        username: 'User',
-        email: 'user@local'
-      },
-      status: 'unauthenticated'
-    }
-  },
-  userAuthed: {
-    title: 'user authenticated',
-    session: {
-      data: {
-        id: 999,
-        login: 'user',
-        role: 'user',
-        roles: ['user'],
-        username: 'User',
-        email: 'user@local'
-      },
-      status: 'authenticated'
-    }
-  }
-};
+import MockSessionContext, { mockAuthStates } from '@tomfreudenberg/next-auth-mock';
 
-export const mockPreviewAuthenticationsToolbar = ({
-  name = 'appAuthenticated',
+/**
+ *
+ * Create the toolbar icon and menu with authStates for mocking.
+ *
+ */
+export const mockAuthPreviewToolbarItem = ({
+  name = 'mockAuthState',
   description = 'Set authentication state',
   defaultValue = null,
   icon = 'user',
-  items = mockPreviewAuthentications
+  items = mockAuthStates
 } = {}) => {
   return {
-    name,
-    description,
-    defaultValue,
-    toolbar: {
-      icon,
-      items: Object.keys(items).map((e) => ({ value: e, title: items[e].title }))
+    mockAuthState: {
+      name,
+      description,
+      defaultValue,
+      toolbar: {
+        icon,
+        items: Object.keys(items).map((e) => ({ value: e, title: items[e].title }))
+      }
     }
   }
 }
+
+/**
+ *
+ * Apply the MockSessionContext as decorator to your storybook preview.
+ *
+ */
+export const withMockAuth = (Story, context) => {
+  const session = React.useMemo(() => {
+    return mockAuthStates[context.globals.mockAuthState]?.session;
+  }, [context.globals.mockAuthState]);
+
+  return React.createElement(MockSessionContext, { session: session }, React.createElement(Story, context));
+};
