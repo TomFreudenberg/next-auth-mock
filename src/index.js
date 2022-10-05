4e41c5ee78d62da73031339c9ca91e006f211311
Tom Freudenberg <tom.freudenberg@4commerce.de>
2022-10-06 01:19:37 +0200

    mod: rewrite component and use use `mock` as identifier

--- src/index.js
+++ src/index.js
@@ -1,19 +1,98 @@
+import React from 'react';
+
 import PropTypes from 'prop-types';
 
 import { SessionContext } from 'next-auth/react';
-import { useMemo } from 'react';
-import { jsx as _jsx } from "react/jsx-runtime";
 
-const SessionContextProvider = ({ children, session }) => {
-  const value = useMemo(() => {
+/**
+ *
+ * default items for toolbar menu to select different auth-states while mocking
+ *
+ */
+export const mockAuthStates = {
+  unknown: {
+    title: 'session unknown',
+    session: null
+  },
+  loading: {
+    title: 'session loading',
+    session: {
+      data: null,
+      status: 'loading'
+    }
+  },
+  admin: {
+    title: 'admin not authenticated',
+    session: {
+      data: {
+        id: 1,
+        login: 'admin',
+        role: 'admin',
+        roles: ['admin', 'user'],
+        username: 'Administrator',
+        email: 'admin@local'
+      },
+      status: 'unauthenticated'
+    }
+  },
+  adminAuthed: {
+    title: 'admin authenticated',
+    session: {
+      data: {
+        id: 1,
+        login: 'admin',
+        role: 'admin',
+        roles: ['admin', 'user'],
+        username: 'Administrator',
+        email: 'admin@local'
+      },
+      status: 'authenticated'
+    }
+  },
+  user: {
+    title: 'user not authenticated',
+    session: {
+      data: {
+        id: 999,
+        login: 'user',
+        role: 'user',
+        roles: ['user'],
+        username: 'User',
+        email: 'user@local'
+      },
+      status: 'unauthenticated'
+    }
+  },
+  userAuthed: {
+    title: 'user authenticated',
+    session: {
+      data: {
+        id: 999,
+        login: 'user',
+        role: 'user',
+        roles: ['user'],
+        username: 'User',
+        email: 'user@local'
+      },
+      status: 'authenticated'
+    }
+  }
+};
+
+/**
+ *
+ * Create a SessionContext based on NextAuth SessionContext to mock useSession()
+ *
+ */
+const MockSessionContext = ({ children, session }) => {
+  const value = React.useMemo(() => {
     return session ? session : { data: undefined, status: "unauthenticated" };
   }, [session]);
-
-  return (_jsx(SessionContext.Provider, { value: value, children: children }));
+  return React.createElement(SessionContext.Provider, { value: value }, children);
 }
 
-SessionContextProvider.propTypes = {
+MockSessionContext.propTypes = {
   children: PropTypes.node.isRequired
 };
 
-export default SessionContextProvider;
+export default MockSessionContext;
